49d119f76139def63f6f4303052634b3f5477844
JHONATAN JESUS ACUÑA HUISACAYNA <acunahuisa@gmail.com>
2022-11-17 17:29:37 -0500

    update amount

--- src/controllers/amount.controller.js
+++ src/controllers/amount.controller.js
@@ -1,26 +1,44 @@
 import { getConnection } from "../database/database";
 
-
-const getAmount = async(req, res) => {
-    try {
-        const {id} = req.params;
-        const connection = await getConnection();
-        const result = await connection.query(
-          "SELECT amoAcc FROM account WHERE idUser=?",
-          id
-        );
-        if (JSON.stringify(result) === "[]") {
-            res.json({ status: "Error", message: "The user doesn't have money" });
-          } else {
-            res.json({ status: "OK", result });
-          }
-        
-      } catch (error) {
-        res.status(500);
-        res.send(error.message);
-      }
-    };
-
-    export const methods = {
-        getAmount
+const getAmount = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const connection = await getConnection();
+    const result = await connection.query(
+      "SELECT amoAcc FROM account WHERE idUser=?",
+      id
+    );
+    if (JSON.stringify(result) === "[]") {
+      res.json({ status: "Error", message: "The user doesn't have money" });
+    } else {
+      res.json({ status: "OK", result });
     }
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+const updateAmount = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { amount } = req.body;
+
+    if (id === undefined || amount === undefined) {
+      res.status(400).json({ message: "Requeste all filed" });
+    }
+    const connection = await getConnection();
+    const result = await connection.query(
+      "UPDATE account SET amoAcc=? WHERE idUser = ?",
+      [amount, id]
+    );
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+export const methods = {
+  getAmount,
+  updateAmount,
+};
